f93a068403df7bc44f9f83afffd4b1d3d2305af9
akash2061 <lucifermorningstar2061@gmail.com>
2024-09-05 00:24:36 +0530

    added musicplayer

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,3 +1,45 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
 public class MusicPlayer {
     private Song currentSong;
+
+    private AdvancedPlayer advancePlayer;
+
+    public MusicPlayer() {
+    }
+
+    public void loadSong(Song song) {
+        currentSong = song;
+        if (currentSong != null) {
+            playCurrentSong();
+        }
+    }
+
+    public void playCurrentSong() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(currentSong.getFilePath());
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+            advancePlayer = new AdvancedPlayer(bufferedInputStream);
+            startMusicThread();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startMusicThread(){
+        new Thread(new Runnable() {
+            @Override
+            public void run(){
+                try {
+                    advancePlayer.play();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 }
